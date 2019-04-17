ad10e7c4e1cbba2223c15439a9d154b790fcbdbb
Enihsuns <enihsunsyxy@gmail.com>
2019-04-17 11:57:50 -0400

    Backend: Spring framework.

---
+++ Backend/src/main/java/com/dbproject/makeup/MakeupApplication.java
@@ -0,0 +1,13 @@
+package com.dbproject.makeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeupApplication.class, args);
+    }
+
+}
