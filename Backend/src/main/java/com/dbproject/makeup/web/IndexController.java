b5aa594314255f30871ff9507b61e9e72ca2d820
Enihsuns <enihsunsyxy@gmail.com>
2019-04-17 13:57:34 -0400

    Backend: Record request info in log files.

--- Backend/src/main/java/com/dbproject/makeup/web/IndexController.java
+++ Backend/src/main/java/com/dbproject/makeup/web/IndexController.java
@@ -2,12 +2,13 @@ package com.dbproject.makeup.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
         return "index";
     }
 }
